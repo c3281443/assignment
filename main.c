8ffee2451697cce4b41464d0f67d7ae962a6c23c
Bill O'Brien <c3281443@uon.edu.au>
2019-04-10 00:04:56 +0000

    lab week 7 mid commit

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 
-void cipher(char *text, int *key);
+void cipher(char text, int key);
 
 int main() {
 
@@ -9,21 +9,21 @@ int main() {
     char text[35]; //this is where he word entered by the user to be encrypted is stored
 
     printf("enter a word to be encrypted : ");
-    gets(text);              //gets() fucntion will read a string and not \o when it reads a space. 
+    scanf( "%s", text);              //$s will read a string and not \o when it reads a space. 
     printf("enter a key: ");
     scanf("%d", &key);
     
-    printf("the encrypted text using a cipher with with a key shift of %d is", key );
+    printf("the encrypted text using a cipher with with a key shift of %d is \n", key );
     
-    cipher(text, key);
+   cipher(text, key);
 
         return 0;
 }
 //encryption using cipher = text + key mod 26. 
-void cipher(char *text, int *key) {
+void cipher(char text, int key) {
     
     int i;  //for the for loop
-    char a;  //ASCII value 
+    int a;  //ASCII value 
     char cipherText;
     
     
@@ -32,6 +32,7 @@ void cipher(char *text, int *key) {
         
         a = ((text[i] - 'a')+ key) % (26 + 'a');
         cipherText = a;
+        printf("%c", cipherText);  //prints the string for main()
     }
     
     
