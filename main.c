4d489d8085e93f8a375207ecbcf95168d6cd0b61
Bill O'Brien <c3281443@uon.edu.au>
2019-04-25 01:04:10 +0000

    25/4 late

--- main.c
+++ main.c
@@ -1,49 +1,47 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h>sssssssssssssssss
+#include<stdlib.h>
 
-void cipher(char *text, int length, int key);
+
+
+void cipher(char *decrypted, int key);
 
 int main() {
 
     int key; //this is the key the user enters 
     char text[35]; //this is where he word entered by the user to be encrypted is stored
-    int length;
+    
 
     printf("enter a word to be encrypted : \n");
     scanf( "%s", text);    //$s will read a string and not \o when it reads a space. 
-    printf("enter a key: \n");
+    printf("enter a key (between 1 and 25): \n");
     scanf("%d", &key);
-    length = strlen(text);
     
     
+    printf("the ciphered text is:  ");
   
-    
-    fdgfd = cipher(text, key, length);
+    cipher(decrypted, key);ddddddddddddddddd
+   
     
   //printf("the encrypted text using a cipher with with a key shift of %d is \n", key );
-        return 0;
+    
 }
 //encryption using cipher = text + key mod 26. 
- cipher(char *text, int length, int key) {
+ void cipher(char *decrypted, int key) {
     
-    int i;  //for the for loop
-    int a;  //ASCII value 
-    char cipherText;
+    int i=0;  //for the for loop
+    int cipherText;
+    int ciphered;
     
-    //loop through the text(string)
-    for (i = 0; i<=length; i++) {
+    while( decrypted[i] != '\0' && strlen(ciphered)-1 > i) {
+        cipherText = ((int)decrypted[i] - 65 + key) % 26 + 65;
+        ciphered = (char)(cipherText);
         
-        a = ((*text)+ key) % 26 + 'a';
-        printf("a = %d\n", a);
-        cipherText = a;
-        printf("%c", cipherText);  //prints the string for main()
-        text++;
-        
+        printf("%c", cipher);
+        i++;
     }
-     printf(" \n");
-     
-    
-    
+            
+    printf("\n");
 }
 
 
